47c0dfe0032878afce0b68f5b560d28a96ff9d3a
Nijat <hatamlinijat@gmail.com>
2026-01-17 19:26:49 +0400

    fix: add docstrings for clarity in helper function and get_page method

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""
+ Simple helper function
+"""
 import csv
 import math
 from typing import List
@@ -32,6 +36,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Implement a method named get_page that takes two integer
+        arguments page with default value 1
+        and page_size with default value 10.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         dataset = self.dataset()
