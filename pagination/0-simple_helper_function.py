da8debf8913f678a74b8efee2cf6e92ae3e4cced
Nijat <hatamlinijat@gmail.com>
2026-01-17 18:43:59 +0400

    add pagination helper function and documentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,11 +3,11 @@
  Simple helper function
 """
 
-def index_range(page, page_size):
+def index_range(page, page_size) -> tuple:
     """
     Return a tuple of size two containing a start
     index and an end index
     """
-    start_page = (page -1) * page_size
+    start_page = (page - 1) * page_size
     end_page = start_page + page_size
     return (start_page, end_page)
