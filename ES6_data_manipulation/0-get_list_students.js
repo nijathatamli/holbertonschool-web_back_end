e1a76ac7604ce4a31b90a8ce68bf1c02131b9e57
Nijat <hatamlinijat@gmail.com>
2026-02-12 12:56:24 -0600

    feat: add getListStudents function to retrieve student data

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents(){
+    const students = [
+        {
+            id : 1,
+            firstName: "Guillaume",
+            location: "San Fransisco"
+        },
+        {
+            id: 2,
+            firstName: "James",
+            location:  "Columbia"
+        },
+        {
+            id: 5,
+            firstName: "Serena",
+            location: "San Fransisco"
+        }
+    ];
+    return students;
+}
